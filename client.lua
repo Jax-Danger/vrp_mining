a75bedff50d2ce31d8ee613f5f1e622bde703941
JaxDanger <jax@jaxdanger.com>
2024-08-10 14:41:33 -0400

    update

--- client.lua
+++ client.lua
@@ -1,30 +1,74 @@
---[[
-Miner Job | Created by JaxDanger
-This File is under fivem's escrow. If you see this, then shame on you for leaking my script.
-]]--
-if not vRP.modules.Jax then return print('modules.Jax doesn\'t exist.') end
+local MiningJob = class("MiningJob", vRP.Extension)
 
--- this module define some Jax tools and functions
-local Jax = class("Jax", vRP.Extension)
-
-function Jax:__construct()
-  vRP.Extension.__construct(self)
-
-  self.miner = false
-
-	local blip = AddBlipForCoord(2942.82, 2785.14, 39.81)
-	SetBlipSprite(blip, 1)
-	SetBlipColour(blip, 1)
-	SetBlipScale(blip, 1.0)
-	SetBlipAsShortRange(blip, true)
-	BeginTextCommandSetBlipName("STRING")
-	AddTextComponentString("Test")
-	EndTextCommandSetBlipName(blip)
+local function prepBlips(self)
+  for k,v in pairs(self.cfg.blips) do
+    local blip = AddBlipForCoord(v.coords[1], v.coords[2], v.coords[3])
+    SetBlipSprite(blip, v.id)
+    SetBlipDisplay(blip, 4)
+    SetBlipScale(blip, v.size)
+    SetBlipColour(blip, v.color)
+    SetBlipAsShortRange(blip, true)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString(v.name)
+    EndTextCommandSetBlipName(blip)
+  end
 end
 
+function MiningJob:__construct()
+  vRP.Extension.__construct(self)
+  self.cfg = module("vrp_mining", "cfg/cfg")
+  prepBlips(self)
 
+  Citizen.CreateThread(function()
+    local isMining = false
+    local targetRock = nil
 
+    -- Thread for checking distance
+    Citizen.CreateThread(function()
+      while true do
+        Citizen.Wait(500) -- Increased wait time to reduce frequency
+        local plCoords = GetEntityCoords(GetPlayerPed(-1))
+        local closestDistance = math.huge
+        targetRock = nil
 
+        for k, v in pairs(self.cfg.rocks) do
+          local x, y, z = v[1], v[2], v[3]
+          local distance = GetDistanceBetweenCoords(x, y, z, plCoords.x, plCoords.y, plCoords.z, true)
+          if distance < closestDistance then
+            closestDistance = distance
+            targetRock = v
+          end
+        end
 
+        if targetRock and closestDistance <= 1.5 then
+          if not isMining then
+            self.remote.mineRocks()
+            isMining = true
+          end
+        elseif isMining and closestDistance > 1.5 then
+          self.remote.stopMining()
+          isMining = false
+        end
+      end
+    end)
 
-vRP:registerExtension(Jax)
+    -- Thread for drawing marker
+    Citizen.CreateThread(function()
+      while true do
+        Citizen.Wait(0) -- Run every frame
+        if targetRock then
+          local playerPed = GetPlayerPed(-1)
+          local plCoords = GetEntityCoords(playerPed)
+          local x, y, z = targetRock[1], targetRock[2], targetRock[3]
+          local distance = GetDistanceBetweenCoords(x, y, z, plCoords.x, plCoords.y, plCoords.z, true)
+
+          if distance <= 2.5 then
+            DrawMarker(1, x, y, z-1, 0, 0, 0, 0, 0, 0, 0.75, 0.75, 0.75, 255, 0, 0, 200, 0, 0, 0, 0)
+          end
+        end
+      end
+    end)
+  end)
+end
+
+vRP:registerExtension(MiningJob)
--- server.lua
+++ server.lua
@@ -1,23 +1,52 @@
-local Jax = class("Jax", vRP.Extension)
-Jax.event = {}
-Jax.tunnel = {}
+local MiningJob = class("MiningJob", vRP.Extension)
+MiningJob.event = {}
+MiningJob.tunnel = {}
 
-function Jax:__construct()
-  vRP.Extension.__construct(self)
-end
-
-function Jax:getID()
+local function mineRock(self)
   local user = vRP.users_by_source[source]
   if user then
-    local identity = vRP.EXT.Identity:getIdentity(user.cid)
-    local fname = identity.firstname
-    local lname = identity.name
-    return {fname, lname}
-  else
-    vRP.EXT.Base.remote._notify(user.source, "You are not logged in. Please relog.")
+    local itemType = "rock"
+    local chanceConfig = self.cfg.chances[itemType]
+
+    if chanceConfig then
+      local randomNumber = math.random(chanceConfig.min, chanceConfig.max)
+      user:tryGiveItem(itemType, randomNumber, false, false)
+      vRP.EXT.Base.remote._notify(user.source, "You've gathered some " .. itemType .. ".")
+    else
+      vRP.EXT.Base.remote._notify(user.source, "Invalid item type.")
+    end
   end
 end
-Jax.tunnel.getID = Jax.getID
+
+function MiningJob:mineRocks()
+  local user = vRP.users_by_source[source]
+  if user then
+    return user:openMenu('rockquarry')
+  end
+end
+MiningJob.tunnel.mineRocks = MiningJob.mineRocks
+
+function MiningJob:stopMining()
+  local user = vRP.users_by_source[source]
+  if user then
+    return user:closeMenus()
+  end
+end
+MiningJob.tunnel.stopMining = MiningJob.stopMining
+
+function MiningJob:__construct()
+  vRP.Extension.__construct(self)
+  self.cfg = module("vrp_mining", "cfg/cfg")
+
+  vRP.EXT.GUI:registerMenuBuilder('rockquarry', function(menu)
+    menu.title = "Rock Quarry"
+    menu.css = {top = "75px", header_color = "rgba(0,125,255,0.75)"}
+
+    menu:addOption('Mine Rock', function(player)
+      return mineRock(self)
+    end)
+  end)
+end
 
 
-vRP:registerExtension(Jax)
+vRP:registerExtension(MiningJob)
